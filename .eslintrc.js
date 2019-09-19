fbb86fd22d994a5a6c8a0289bdfe7169adca4f15
Waldek <waldek.pieniak@gmail.com>
2019-09-19 18:43:36 +0200

    Test commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: 'piecioshka',
+
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+    },
+    rules: {},
+    globals: {},
+    parserOptions: {}
+};
